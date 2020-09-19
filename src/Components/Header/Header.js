a1d0c37dd85e17d49807f61a45db3612d3596ab4
rashedhero <rashedbefresh@gmail.com>
2020-09-19 23:10:43 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+// import { Button, Form, FormControl, Image, Nav, Navbar } from 'react-bootstrap';
+
+// import logo from './logo.svg';
+// import './Header.css';
+
+const Header = () => {
+    return (
+      
+     
+      <div className="main_div">
+
+      <nav className="nav">
+                <ul>
+                    <li>
+                        {/* <img className="logo" src={logo} alt=""/> */}
+                    </li>
+                    <li>
+                      News
+                    </li>
+                    <li>
+                        Destination
+                    </li>
+                    <li>
+                        blog
+                    </li>
+                    <li>
+                        contact
+                    </li>
+                </ul>
+            </nav>
+
+          
+      </div>
+      
+
+     
+       
+       
+
+    );
+};
+
+export default Header;
